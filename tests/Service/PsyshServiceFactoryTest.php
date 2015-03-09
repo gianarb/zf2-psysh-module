d3133e1bf81f5a69c413940b0ee97cbb87ba754a
Gianluca Arbezzano <gianarb92@gmail.com>
2015-03-09 22:56:40 +0100

    Fix PsyshServiceFactory

--- tests/Service/PsyshServiceFactoryTest.php
+++ tests/Service/PsyshServiceFactoryTest.php
@@ -2,11 +2,22 @@
 namespace PsyshModuleTest\Service;
 
 use Zend\Test\PHPUnit\Controller\AbstractControllerTestCase;
+use Zend\ServiceManager\ServiceManager;
 
 class PsyshServiceFactoryTest extends AbstractControllerTestCase
 {
-    public function testPing()
+    public function testFactoryReturnPsyshServiceInstance()
     {
-        $this->assertTrue(true);
+        $sm = new ServiceManager();
+        $sm->setService(
+            'Config',
+            array(
+                'psysh' => array(
+                    'enable' => true
+                ),
+            )
+        );
+        $sm->setFactory('psysh', 'PsyshModule\Service\PsyshServiceFactory');
+        $this->assertInstanceOf('PsyshModule\Service\PsyshService', $sm->get("psysh"));
     }
 }
